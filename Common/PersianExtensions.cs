0e2866eb65a4b1e0a877a8c6aad1df26c7e987bf
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-07-28 23:07:34 +0430

    #add extensions #add db context

---
+++ Common/PersianExtensions.cs
@@ -0,0 +1,105 @@
+﻿using DNTPersianUtils.Core;
+
+namespace Common
+{
+    public static class PersianExtensions
+    {
+        /// تبدیلگر عدد به حروف	
+        public static string ConvertNumberToText(this int value)
+        {
+            return value.NumberToText(Language.Persian);
+        }
+
+        /// تبدیلگر عدد به حروف	
+        public static string ConvertNumberToText(this long value)
+        {
+            return value.NumberToText(Language.Persian);
+        }
+
+        //نمایش دوستانه‌ی یک تاریخ و ساعت انگلیسی به شمسی	
+        public static string ConvertToFriendlyPersianDateTextify(this DateTime value)
+        {
+            return value.ToFriendlyPersianDateTextify(); //‫۱۰ روز قبل، سه شنبه ۲۱ دی ۱۳۹۵، ساعت ۱۰:۲۰
+        }
+
+        //نمایش فارسی روز دریافتی		
+        public static string ConvertToPersianDateTextify(this DateTime value)
+        {
+            return value.ToPersianDateTextify(); //‫سه شنبه ۲۱ دی ۱۳۹۵
+        }
+
+        //تبدیل تاریخ میلادی به شمسی			
+        public static string ConvertToLongPersianDateString(this DateTime value)
+        {
+            return value.ToLongPersianDateString(); //‫‫21 دی 1395
+        }
+
+        //تبدیل تاریخ میلادی به شمسی			
+        public static string ConvertToShortPersianDateString(this DateTime value)
+        {
+            return value.ToShortPersianDateString(); //1395/10/21
+        }
+
+        //تبدیل تاریخ میلادی به شمسی			
+        public static string ConvertToShortPersianDateTimeString(this DateTime value)
+        {
+            return value.ToShortPersianDateTimeString(); //1395/10/21 10:20
+        }
+
+        //تبدیل تاریخ و زمان رشته‌ای شمسی به میلادی با قالب‌های پشتیبانی شده‌ی 1395/12/30		
+        public static DateTime? ConvertToGregorianDateTime(this string value)
+        {
+            return value.ToGregorianDateTime();
+        }
+
+        /// تعیین اعتبار تاریخ و زمان رشته‌ای شمسی		
+        public static bool CheckIsValidPersianDateTime(this string value)
+        {
+            return value.IsValidPersianDateTime();//1395/12/30
+        }
+
+        /// بررسی اعتبار کد ملی			
+        public static bool CheckIsValidIranianNationalCode(this string value)
+        {
+            return value.IsValidIranianNationalCode();//0010350829
+        }
+
+        /// بررسی اعتبار شناسه ملی حقوقی				
+        public static bool CheckIsValidIranianNationalLegalCode(this string value)
+        {
+            return value.IsValidIranianNationalLegalCode();//14005893875
+        }
+
+        /// بررسی اعتبار شماره موبایل			
+        public static bool CheckIsValidIranianMobileNumber(this string value)
+        {
+            return value.IsValidIranianMobileNumber();//09901464762
+        }
+
+        /// بررسی اعتبار شماره تلفن			
+        public static bool CheckIsValidIranianPhoneNumber(this string value)
+        {
+            return value.IsValidIranianPhoneNumber();//37236445
+        }
+
+        /// بررسی اعتبار کد پستی				
+        public static bool CheckIsValidIranianPostalCode(this string value)
+        {
+            return value.IsValidIranianPostalCode();//1619735744
+        }
+
+        /// بررسی اعتبار کد بانک شبا				
+        public static bool CheckIsValidIranShebaNumber(this string value)
+        {
+            return value.IsValidIranShebaNumber(); //IR820540102680020817909002
+        }
+
+        /// بررسی اعتبار کد بانک شتاب				
+        public static bool CheckIsValidIranShetabNumber(this string value)
+        {
+            return value.IsValidIranShetabNumber(); //6221061106498670
+        }
+
+
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,95 @@
+﻿using Common;
+using Domain;
+using Domain.AuditableEntity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Reflection;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var entitiesAssembly = typeof(IEntity).Assembly;
+
+            modelBuilder.RegisterAllEntities<IEntity>(entitiesAssembly);
+            modelBuilder.RegisterEntityTypeConfiguration(entitiesAssembly);
+            modelBuilder.AddRestrictDeleteBehaviorConvention();
+            modelBuilder.AddAuditableShadowProperties();
+        }
+
+        public override int SaveChanges()
+        {
+            beforeSaveTriggers();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            beforeSaveTriggers();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            beforeSaveTriggers();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            beforeSaveTriggers();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void _cleanString()
+        {
+            var changedEntities = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+            foreach (var item in changedEntities)
+            {
+                if (item.Entity == null)
+                    continue;
+
+                var properties = item.Entity.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.CanRead && p.CanWrite && p.PropertyType == typeof(string));
+
+                foreach (var property in properties)
+                {
+                    var propName = property.Name;
+                    var val = (string)property.GetValue(item.Entity, null);
+
+                    if (val.HasValue())
+                    {
+                        var newVal = val.Fa2En().FixPersianChars();
+                        if (newVal == val)
+                            continue;
+                        property.SetValue(item.Entity, newVal, null);
+                    }
+                }
+            }
+        }
+
+
+        private void beforeSaveTriggers()
+        {
+            _cleanString();
+            setShadowProperties();
+        }
+
+        private void setShadowProperties()
+        {
+            // we can't use constructor injection anymore, because we are using the `AddDbContextPool<>`
+            var props = this.GetService<IHttpContextAccessor>()?.GetShadowProperties();
+            ChangeTracker.SetAuditableEntityPropertyValues(props);
+        }
+    }
+}
