e225e2bad806ae215f1a12adc64f07a27bb427a3
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-07-30 16:48:31 +0430

    #add product repository

---
+++ Data/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Data.Contracts;
+using Domain;
+
+namespace Data.Repositories
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext dbContext)
+         : base(dbContext)
+        {
+        }
+    }
+}
