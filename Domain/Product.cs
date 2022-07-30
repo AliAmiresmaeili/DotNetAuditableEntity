6c244d1efa874ed1cb07c8fe084f67ce3f31d067
Ali Amiresmaeili <aliamiresmaeili@gmail.com>
2022-07-30 16:22:40 +0430

    #add product model

---
+++ Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int? CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category { get; set; }
+
+    }
+}
